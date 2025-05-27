a701ef073f8cc4911f329a6b7a7b5e1f4c3f771f
Aditya <dit7572@gmail.com>
2025-05-27 19:21:49 +0700

    (feat) add sfx

---
+++ audio.js
@@ -0,0 +1,25 @@
+
+var sound1 = new Audio();
+sound1.src="audio/i-can-do-this-all-day-captain-america.mp3"
+
+
+var sound2 = new Audio();
+sound2.src="audio/Voicy_I am iron man.mp3"
+
+var sound3 = new Audio();
+sound3.src="audio/bring_me_thanos.mp3"
+
+var sound4 = new Audio();
+sound4.src="audio/that's-my-secret-captain-i'm-always-angry.mp3"
+
+var sound5 = new Audio();
+sound5.src="audio/Voicy_I got red in my ledger, I'd like to wipe it out..mp3"
+
+var sound6 = new Audio();
+sound6.src="audio/Voicy_What if I say no_.mp3"
+
+var sound7 = new Audio();
+sound7.src="audio/Voicy_We're The Avengers Man.mp3"
+
+var sound8 = new Audio();
+sound8.src="audio/i-am-inevitable_7kiktVD.mp3"
